68a37bdbe39237111142453270c68d1e78f7b8e7
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2021-05-27 16:31:33 -0300

    parte 2 do dado

--- from time import sleep.py
+++ from time import sleep.py
@@ -1,3 +1,4 @@
+from random import randint
 from time import sleep
 print('Vamos jogar o dado')
 sleep(3)
@@ -7,3 +8,5 @@ print('2')
 sleep(1)
 print('3')
 sleep(1)
+n1 = randint(1,6)
+print('O dado caiu na opção {}'.format(n1))
