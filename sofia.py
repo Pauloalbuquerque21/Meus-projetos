7353641644c43c2d942546e1c0f47cc7ff8db808
Paulo Vitor Albuquerque silva <paulo.vitor21@hotmail.com>
2021-06-08 01:24:07 -0300

    aprendendo calcular

--- sofia.py
+++ sofia.py
@@ -5,4 +5,9 @@ n1 = int(input('Você precisa de ajuda ?\nMatemática[1]\nPortuguês[2]\nEscolha
 n2 = [1,2]
 while n1 != 1 and n1!=2:
     n1 = int(input('Não entendi, matemática[1] ou português[2]?'))
-
+if n1 == 1:
+    print('=-'*20)
+    n3 = int(input('O escolhido foi Matemática.\nCalculadora[1]\nEscolhe uma das opções:'))
+        if n3 == 1:
+            print('=-'*20)
+            n4 = ('[1]Soma\n[2]Multiplicação\n[3]divisão')
