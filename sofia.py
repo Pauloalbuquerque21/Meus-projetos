fa442a687899da6cee932b90d7c059c7bcba250c
Paulo Vitor Albuquerque silva <paulo.vitor21@hotmail.com>
2021-06-11 22:05:41 -0300

    Fazendo a PA

--- sofia.py
+++ sofia.py
@@ -7,9 +7,11 @@ while n1 != 1 and n1!=2:
     n1 = int(input('Não entendi, matemática[1] ou português[2]?'))
 if n1 == 1:
     print('=-'*20)
-    n3 = int(input('O escolhido foi Matemática.\nCalculadora[1]\nEscolhe uma das opções:'))
+    n3 = int(input('O escolhido foi Matemática.\nCalculadora[1]\nProgreção Aritimetica[2]\nEscolhe uma das opções:'))
     if n3 == 1:
-        print('=-'*20)
+        print('-=-'*20)
+        print('CALCULADORA')
+        print('-=-'*20)
         n4 = int(input('[1]Soma\n[2]Multiplicação\n[3]divisão\nEscolha umas das opções:'))
         if n4 == 1:
             n5 = int(input('quantos números vão ser somados ?'))
@@ -33,3 +35,13 @@ if n1 == 1:
             n10 = n8 / n9
             n11 = n8 % n9
             print('O resultado da divisão {:.2f} e o resto {:.2f}'.format(n10,n11))
+    elif n3 == 2:
+        print('-=-'*20)
+        print('PROGREÇÃO ARITIMÉTICA')
+        print('-=-'*20)
+        sleep(2)
+        print('\033[32mFomula:\nan=a1+(n-1).r\033[m')
+        print('-=-'*20)
+        sleep(2)
+        n12 = int(input('Informações:\nPrimeiro termo (a1) [1]\nUltimo termo (an) [2]\nRazão (n) [3]\nNúmero de termos (n) [4]\nQuais dessas informações você tem?'))
+                
