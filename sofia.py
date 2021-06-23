71ad82a40abc92ba7e5fa9de90c1f826ae7bf871
Paulo Vitor Albuquerque silva <paulo.vitor21@hotmail.com>
2021-06-22 23:43:08 -0300

    pequenos ajustes, con as frases antes do while

--- sofia.py
+++ sofia.py
@@ -1,13 +1,13 @@
 from time import sleep
 print('Ola amigo, bom dia!!!!')
 sleep(1)
-n1 = int(input('Você precisa de ajuda ?\nMatemática[1]\nPortuguês[2]\nEscolha uma das opções que deseja ajuda:'))
+n1 = 0
 n2 = [1,2]
 while n1 != 1 and n1!=2:
     n1 = int(input('Não entendi, matemática[1] ou português[2]?'))   
 if n1 == 1:
     print('=-'*20)
-    n3 = int(input('O escolhido foi Matemática.\nCalculadora[1]\nProgreção Aritimetica[2]\nEscolhe uma das opções:'))
+    n3 = 0
     while  n3 !=1 and n3!=2:
         n3 = int(input('O escolhido foi Matemática.\nCalculadora[1]\nProgreção Aritimetica[2]\nEscolhe uma das opções:'))
         if n3 == 1:
