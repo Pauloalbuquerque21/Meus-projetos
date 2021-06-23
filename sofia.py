affa8626882c0e4004365059b27128d943bcf231
Paulo Vitor Albuquerque silva <paulo.vitor21@hotmail.com>
2021-06-23 20:57:47 -0300

    terminei o while true, agora quando termina volta para o inicio e quando quer sair só clicar 10

--- sofia.py
+++ sofia.py
@@ -4,8 +4,8 @@ sleep(1)
 n1 = 0
 n2 = [1,2]
 while True:
-    while n1 != 1 and n1!=2:
-        n1 = int(input('Escolha uma das opções:\nMatemática[1]\nPortuguês[2]\nQual?'))   
+    n1 = int(input('Escolha uma das opções:\nMatemática[1]\nPortuguês[2]\nSair[10]\nQual?'))
+    sleep(1)
     if n1 == 1:
         print('=-'*20)
         n3 = 0
@@ -21,7 +21,7 @@ while True:
                     f = 0
                     for c in range(1,n5+1):
                         n6 = float(input('Número nº {}:'.format(c)))
-                        f= f + n6  
+                        f= f + n6
                     print('O resultado da soma é igual: {:.2f}'.format(f))
                 elif n4 == 2:
                     print('-=-'*20)
@@ -33,8 +33,8 @@ while True:
                     print('O valor da Multiplicação é {:.2f}'.format(g))
                 elif n4 == 3:
                     print('-=-'*20)
-                    n8 = float(input('Informe o Dividendo:'))      
-                    n9 = float(input('Informe o divisor:')) 
+                    n8 = float(input('Informe o Dividendo:'))
+                    n9 = float(input('Informe o divisor:'))
                     n10 = n8 / n9
                     n11 = n8 % n9
                     print('O resultado da divisão {:.2f} e o resto {:.2f}'.format(n10,n11))
@@ -53,14 +53,14 @@ while True:
                     if  n12 in 'abc' or 'acb' or 'bac' or 'bca' or 'cab' or 'cba':
                         n14 = int(input('Informe o Primeiro termo:'))
                         n15 = int(input('Informe o ultimo termo: '))
-                        n16 = int(input('informe a razão:'))    
+                        n16 = int(input('informe a razão:'))
                         pa1 = 1+((n15-n14)/n16)
                         print('O número de termos e igual {}'.format(pa1))
                         perg = str(input('Você quer ver todos os termos[S/N]?')).strip().lower()
                         if perg == 's':
                             print('Segue os termos:')
                             for c in range(n14,n15,n16):
-                                print('{}'.format(c),end=' ')        
+                                print('{}'.format(c),end=' ')
                     elif n12 in 'bcd' or 'bdc' or 'cbd' or 'cdb' or 'dcb' or 'dbc':
                         n15 = int(input('Informe o Ultimo termo:'))
                         n16 = int(input('Informe a razão:'))
@@ -73,4 +73,6 @@ while True:
                         n21 = int(input('informe o primeiro termo:'))
                         n22 = int(input('Informe o ultimo termo:'))
                         n23 = int(input('Informe o número de termos:'))
-                
+    if n1 == 10:
+        break
+                    
