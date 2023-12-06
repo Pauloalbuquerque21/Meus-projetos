aed37611480e86fd5312decd85ee50eeb0de5edd
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2023-12-05 22:39:53 -0300

    indo para segunda fase

--- organizador/recusividade_hashtag.py
+++ organizador/recusividade_hashtag.py
@@ -6,13 +6,31 @@ def transform(dados):
         if ('.png' in c) or ('jpg' in c) or ('jpeg' in c):
             inf = inf + 1
             if 'png' in c:
-                os.rename(f'{c}',f'{inf}.png')
+                while True:
+                    if f'{inf}.png' in dados:
+                        print(f'existente: {inf}.png')
+                        inf +=1
+                    else:
+                        os.rename(f'{c}',f'{inf}.png')
+                        break
             elif 'jpg' in c:
-                os.rename(f'{c}',f'{inf}.jpg')
+                while True:
+                    if f'{inf}.jpg' in dados:
+                        print(f'existente: {inf}.jpg')
+                        inf +=1
+                    else:
+                        os.rename(f'{c}',f'{inf}.jpg')
+                        break
             elif 'jpeg' in c:
-                os.rename(f'{c}',f'{inf}.jpeg')
+                while True:
+                    if f'{inf}.jpeg' in dados:
+                        print(f'existente: {inf}.jpeg')
+                        inf +=1
+                    else:
+                        os.rename(f'{c}',f'{inf}.jpeg')
+                        break
 while True:
-    caminho = input('Digite o caminho:')
+    caminho = input('Digite o caminho:').strip()
     os.chdir(caminho)
     dados_arquivo = os.listdir() 
     print(dados_arquivo)
--- text.py
+++ text.py
@@ -1,27 +1,7 @@
-d = [[2,3],[4,6]]
-c = [[1,3,0],[2,1,1]]
+inf = 1
+c = '1.jpg'
 
-
-def multiplicaMatrizes(a,b):
-    mult = 0
-    matriz_result_mult = list()
-    if len(a[0]) == len(b):
-        for linhas in range(0,len(a)):
-            test = list()
-            for colunas in range(0,len(b[0])):
-                test.append(0)
-            matriz_result_mult.append(test)
-        for c in range(0,len(a)):
-            for c3 in range(0,len(b[0])):
-                result = 0
-                for c2 in range(0,len(a[0])):
-                    mult = a[c][c2] * b[c2][c3]
-                    result = mult + result
-                matriz_result_mult[c][c3] = result
-        result_mult = matriz_result_mult
-    else:
-        result_mult = 'Inexistente!!'
-    return result_mult
-
-x = multiplicaMatrizes(d,c)
-print(x)
+if c == f'{inf}.jpg':
+    print('certo')
+else:
+    print('errado')
